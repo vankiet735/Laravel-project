b2c58fb865d1bccaeeb20c1a8a1e8d33f5cd7cce
thanhduoc2311 <50187527+thanhduoc2311@users.noreply.github.com>
2019-11-09 15:53:41 +0700

    hihi

--- demo/resources/views/layout.blade.php
+++ demo/resources/views/layout.blade.php
@@ -1,8 +1,9 @@
-<!DOCTYPE html>
+﻿<!DOCTYPE html>
+
 <html lang="en">
     <head>
         <meta charset="utf-8" />
-        <title>Upvex - Responsive Admin Dashboard Template</title>
+        <title>Upvex - Responsive Admin Dashboard Template  ^^^</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta content="A fully featured admin theme which can be used to build CRM, CMS, etc." name="description" />
         <meta content="Coderthemes" name="author" />
